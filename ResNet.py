914631ee0bc3d7be3f183904e7f423d80b68a190
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-03-25 18:05:38 -0500

    Added contrastive loss training

--- ResNet.py
+++ ResNet.py
@@ -14,7 +14,7 @@ def resnet_34(pretrained=True):
     return model, feature_dims, 1024
 
 def resnet_50(pretrained=True):
-    model = models_2d.resnet50(pretrained=pretrained)
+    model = models_2d.resnet50(pretrained=False)
     feature_dims = model.fc.in_features
     model.fc = Identity()
     return model, feature_dims, 1024
