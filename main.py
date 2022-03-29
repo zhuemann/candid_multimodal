1bb70e335bf795cc48c186c67c66b40b8197f4c3
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-03-29 17:43:47 -0500

    Added contrastive loss training

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
         directory_base = "/UserData/"
 
 
-    training_loop(seed = 7, batch_size = 32, dir_base= directory_base, epoch = 50, n_classes = 2)
+    training_loop(seed = 7, batch_size = 16, dir_base= directory_base, epoch = 50, n_classes = 2)
 
     #load_best_model(dir_base= directory_base)
 
