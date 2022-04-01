e9785b849e2088ec2d33c8346fc720ed39bf8e37
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-03-31 23:14:58 -0500

    Hopefully fixed bug that prevented training

--- contrastive_training.py
+++ contrastive_training.py
@@ -179,9 +179,9 @@ def training_loop(seed, batch_size=8, epoch=1, dir_base = "/home/zmh001/r-fcb-is
         save_path = os.path.join(dir_base, 'Zach_Analysis/models/smp_models/default_from_smp/vgg19')
         torch.save(model_obj.state_dict(), save_path)
     else:
-        model_obj = smp.Unet(encoder_name="resnet34", encoder_weights=None, in_channels=1, classes=1) #timm-efficientnet-b8 resnet34
+        model_obj = smp.Unet(encoder_name="resnet50", encoder_weights=None, in_channels=1, classes=1) #timm-efficientnet-b8 resnet34
         save_path = os.path.join(dir_base, 'Zach_Analysis/models/smp_models/default_from_smp_three_channel/resnet50')
-        model_obj.load_state_dict(torch.load(save_path))
+        #model_obj.load_state_dict(torch.load(save_path))
 
     #save_path = os.path.join(dir_base, 'Zach_Analysis/models/resnet34/default_from_smp/resnet152')
     #torch.save(model_obj.state_dict(), save_path)
