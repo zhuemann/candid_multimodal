6710992dea23c15b5f3b5de192a193c6e34489f1
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-03-31 23:57:41 -0500

    Hopefully fixed bug that prevented training

--- dataloader_image_text.py
+++ dataloader_image_text.py
@@ -42,7 +42,7 @@ class TextImageDataset(Dataset):
             None,
             add_special_tokens=True,
             max_length=self.max_len,
-            #pad_to_max_length=True,
+            pad_to_max_length=True,
             #padding='longest',
             #truncation='True'
             return_token_type_ids=True
