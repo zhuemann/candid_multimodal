7c2cf504a6cd4d5cddaba8bec16a9d845acfd9a0
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-04-04 14:26:38 -0500

    Implementing Gloria Code

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ if __name__ == '__main__':
     else:
         directory_base = "/UserData/"
 
-    pretrained_model = contrastive_pretraining(seed = 7, batch_size = 16, dir_base= directory_base, epoch = 30, n_classes = 2)
-    #pretrained_model = None
+    #pretrained_model = contrastive_pretraining(seed = 7, batch_size = 16, dir_base= directory_base, epoch = 30, n_classes = 2)
+    pretrained_model = None
     #model_obj = load_img_segmentation_model()
 
     #load_best_model(dir_base= directory_base)
