5e222dc37b06638ec3dc4cf12496f12b21c9f4e2
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-04-04 12:55:31 -0500

    Implementing Gloria Code

--- create_unet.py
+++ create_unet.py
@@ -51,11 +51,12 @@ def load_img_segmentation_model(dir_base = "/UserData/", pretrained_model = None
             name = k[6:]  # remove `model.`
             new_state_dict[name] = v
         # load params
-        new_state_dict = new_state_dict.pop("_embedder.weight", None)
-        new_state_dict = new_state_dict.pop("_embedder.bias", None)
-        new_state_dict = new_state_dict.pop("embedder.weight", None)
-        #new_state_dict["_embedder.bias"] = None
-        #new_state_dict["embedder.weight"] = None
+        #new_state_dict = new_state_dict.pop("_embedder.weight", None)
+        #new_state_dict = new_state_dict.pop("_embedder.bias", None)
+        #new_state_dict = new_state_dict.pop("embedder.weight", None)
+        new_state_dict["_embedder.bias"] = None
+        new_state_dict["embedder.weight"] = None
+        del new_state_dict["_embedder.weight"]
 
         seg_model.encoder.load_state_dict(new_state_dict)
 
