90547d0897eee5a7ecb37c3687d20c3d2628b9e1
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-04-05 11:06:52 -0500

    Pretrain on all data

--- contrastive_training.py
+++ contrastive_training.py
@@ -59,9 +59,9 @@ def contrastive_pretraining(seed, batch_size=8, epoch=1, dir_base = "/home/zmh00
     #df.to_excel(dataframe_location, index=False)
 
     # reads in the dataframe as it doesn't really change to save time
-    df = pd.read_excel(dataframe_location, engine='openpyxl')
+    train_df = pd.read_excel(dataframe_location, engine='openpyxl')
     #print(df)
-    df.set_index("image_id", inplace=True)
+    train_df.set_index("image_id", inplace=True)
     #print(df)
 
 
@@ -79,9 +79,9 @@ def contrastive_pretraining(seed, batch_size=8, epoch=1, dir_base = "/home/zmh00
     # df = truncate_left_text_dataset(df, tokenizer)
 
     # Splits the data into 80% train and 20% valid and test sets
-    train_df, test_valid_df = model_selection.train_test_split(
-        df, test_size=0.0, random_state=seed, shuffle=True #stratify=df.label.values
-    )
+    #train_df, test_valid_df = model_selection.train_test_split(
+    #    df, test_size=0.0, random_state=seed, shuffle=True #stratify=df.label.values
+    #)
     # Splits the test and valid sets in half so they are both 10% of total data
     #test_df, valid_df = model_selection.train_test_split(
     #    test_valid_df, test_size=0.5, random_state=seed, shuffle=True #stratify=test_valid_df.label.values
