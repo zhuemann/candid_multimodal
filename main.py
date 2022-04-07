1336a55a9a5d28f70e12c9b5ee7e627f25917356
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-04-07 12:48:26 -0500

    Run gloria segmentation 50epochs on siim data

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ if __name__ == '__main__':
         matrix = acc
         df = pd.DataFrame(valid_log)
         df["test_accuracy"] = acc
-        #file_name = 'pretraining_vision_run'
-        file_name = 'image_net_weights'
+        file_name = 'pretraining_vision_run'
+        #file_name = 'image_net_weights'
         ## save to xlsx file
         filepath = os.path.join(directory_base,
                                 '/UserData/Zach_Analysis/result_logs/candid_result/reimplementation/' + str(file_name) +'/valid_run_seed' + str(
