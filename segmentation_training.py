5556c093cfb9de8767696012c95dcf0042836cd4
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-04-07 12:28:32 -0500

    Run imagenet segmentation 50epochs on siim data

--- segmentation_training.py
+++ segmentation_training.py
@@ -144,11 +144,11 @@ def segmentation_training(seed, batch_size=8, epoch=1, dir_base = "/home/zmh001/
 
         # Splits the data into 80% train and 20% valid and test sets
         train_df, test_valid_df = model_selection.train_test_split(
-            df, test_size=0.95, random_state=seed, shuffle=True  # stratify=df.label.values
+            df, test_size=0.96, random_state=seed, shuffle=True  # stratify=df.label.values
         )
         # Splits the test and valid sets in half so they are both 10% of total data
         test_df, valid_df = model_selection.train_test_split(
-            test_valid_df, test_size=0.1, random_state=seed, shuffle=True  # stratify=test_valid_df.label.values
+            test_valid_df, test_size=0.05, random_state=seed, shuffle=True  # stratify=test_valid_df.label.values
         )
         print("train_df")
         print(train_df)
