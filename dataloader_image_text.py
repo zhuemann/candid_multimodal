b655ea8425fdb391eb1a6fcb181bbb734f9abb59
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-05-18 16:12:33 -0500

    Adding ConTextual segmentation

--- dataloader_image_text.py
+++ dataloader_image_text.py
@@ -106,7 +106,7 @@ class TextImageDataset(Dataset):
                     img = Image.fromarray(img)
                     img = np.array(img)
                     # segmentation_mask_org = np.uint8(segmentation_mask_org)
-                    # print(type(img))
+                    print(type(img))
                     transformed = self.transforms(image=img, mask=segmentation_mask_org)
                     image = transformed['image']
                     segmentation_mask_org = transformed['mask']
@@ -114,14 +114,15 @@ class TextImageDataset(Dataset):
                     image = self.resize(image)  # resizes the image to be the same as the model size
                     # segmentation_mask = Image.fromarray(np.uint8(segmentation_mask))
                     # segmentation_mask = self.resize(segmentation_mask)
-                    print(type(image))
-                    print(image.size())
+
 
                 else:
                     # img = Image.fromarray(img).convert("RGB")
                     img = Image.fromarray(img)
                     img = np.array(img)
                     # image = Image.fromarray(img)  # makes the image into a PIL image
+                    print(type(img))
+                    print(img.size())
                     image = self.transforms(img)
                     print(type(image))
                     print(image.size())
