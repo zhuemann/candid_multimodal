3f06a1b4161e8725d00fe0e05810caadbb8afe63
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-05-19 12:07:51 -0500

    Adding ConTextual segmentation

--- ConTEXTual_Segmentation_model.py
+++ ConTEXTual_Segmentation_model.py
@@ -23,7 +23,8 @@ class ConTEXTual_seg_model(torch.nn.Module):
         factor = 2 if bilinear else 1
         self.down4 = Down(512, 1024 // factor)
 
-        self.up0 = Up(1024, 1024 // factor, bilinear)
+        self.combine = DoubleConv(2048, 1024)
+        #self.up0 = Up(1024, 1024 // factor, bilinear)
         self.up1 = Up(1024, 512 // factor, bilinear)
         self.up2 = Up(512, 256 // factor, bilinear)
         self.up3 = Up(256, 128 // factor, bilinear)
@@ -52,8 +53,9 @@ class ConTEXTual_seg_model(torch.nn.Module):
         #print(x4.size())
         x5 = self.down4(x4)
         #print(x5.size())
-        #joint_rep = torch.cat((x5, lang_rep), dim=1)
+        joint_rep = torch.cat((x5, lang_rep), dim=1)
 
+        x5 = self.combine(joint_rep)
         #print(x5.size())
         #print(lang_rep.size())
 
@@ -61,8 +63,8 @@ class ConTEXTual_seg_model(torch.nn.Module):
         #x = self.up1(x, x4)
 
         #x = self.up1(zeros, x4)
-        x = self.up1(lang_rep, x4)
-        #x = self.up1(x5, x4)
+        #x = self.up1(lang_rep, x4)
+        x = self.up1(x5, x4)
 
         x = self.up2(x, x3)
         x = self.up3(x, x2)
