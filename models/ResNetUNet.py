a093e8400aae827d81a6e83e943de152e69f97e1
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-05-19 21:36:45 -0500

    Adding ConTextual with resnet segmentation

--- models/ResNetUNet.py
+++ models/ResNetUNet.py
@@ -57,6 +57,7 @@ class ResNetUNet(nn.Module):
         layer4 = self.layer4(layer3)
 
         layer4 = self.layer4_1x1(layer4)
+        print(layer4.size())
         x = self.upsample(layer4)
         layer3 = self.layer3_1x1(layer3)
         x = torch.cat([x, layer3], dim=1)
