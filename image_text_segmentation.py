cb510ef98cecd3bf4264a1a31f46cb191ff84a77
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-05-19 23:46:22 -0500

    Adding ConTextual with resnet segmentation

--- image_text_segmentation.py
+++ image_text_segmentation.py
@@ -87,11 +87,11 @@ def train_image_text_segmentation(config, batch_size=8, epoch=1, dir_base = "/ho
 
     # Splits the data into 80% train and 20% valid and test sets
     train_df, test_valid_df = model_selection.train_test_split(
-        df, train_size=0.8, random_state=seed, shuffle=True #stratify=df.label.values
+        df, train_size=config["train_samples"], random_state=seed, shuffle=True #stratify=df.label.values
     )
     # Splits the test and valid sets in half so they are both 10% of total data
     test_df, valid_df = model_selection.train_test_split(
-        test_valid_df, test_size=0.5, random_state=seed, shuffle=True #stratify=test_valid_df.label.values
+        test_valid_df, test_size=config["test_samples"], random_state=seed, shuffle=True #stratify=test_valid_df.label.values
     )
 
     #test_dataframe_location = os.path.join(dir_base, 'Zach_Analysis/candid_data/pneumothorax_df_testset.xlsx')
