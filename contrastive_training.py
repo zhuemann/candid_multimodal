28201e943c2048ca42d3ee151b0da0738534d77b
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-06-07 22:56:29 -0500

    Testing attention maps in gloria

--- contrastive_training.py
+++ contrastive_training.py
@@ -234,7 +234,7 @@ def contrastive_pretraining(config):
                 print(im)
                 print(type(im))
                 print(np.shape(im[0]))
-                im = Image.fromarray(im[0])
+                im = Image.fromarray(im[0], 'RGB')
 
                 # f, ax = plt.subplots(1, 3)
                 # ax[0].imshow(img_raw, cmap=plt.cm.bone)
