09e3744f0248cf427e766c1c47d2cabe7c8226d3
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-06-17 11:15:47 -0500

    Testing multipliccative attention in segmentation model

--- image_text_segmentation.py
+++ image_text_segmentation.py
@@ -281,11 +281,14 @@ def train_image_text_segmentation(config, batch_size=8, epoch=1, dir_base = "/ho
 
             #testing delete later
             lang_output = language_model(ids, mask, token_type_ids)
-            print("output size")
-            print(lang_output.size())
             print("testing langauge model in outer loop")
+            lang_rep = lang_output[0]
+            print(torch.isnan(lang_rep).any())
             lang_rep = lang_output[1]
             print(torch.isnan(lang_rep).any())
+            lang_rep = lang_output[2]
+            print(torch.isnan(lang_rep).any())
+            print("done")
 
 
             outputs = test_obj(images, ids, mask, token_type_ids)
