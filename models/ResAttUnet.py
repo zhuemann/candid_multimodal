d7b66c6cd5ae05571cae1565f2baabc3d061b486
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-06-22 15:18:26 -0500

    Testing resnet with spatial attention encoder

--- models/ResAttUnet.py
+++ models/ResAttUnet.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 from torchvision import models
+import torch.nn.functional as F
+
 import os
 
 def convrelu(in_channels, out_channels, kernel, padding):
@@ -76,6 +78,33 @@ class ResAttNetUNet(nn.Module):
 
 
 
+        bilinear = False
+        # layers from other versino
+        self.up1 = Up(1024, bilinear)
+        self.attention1 = Attention_block(512, 512, 256)
+        # self.multiplicativeAttention = DotProductAttention(hidden_dim=10)
+        # self.multihead_attn = nn.MultiheadAttention(embed_dim=1024, num_heads=1)
+        self.lang_attn = LangCrossAtt(emb_dim=1024)
+        self.up_conv1 = DoubleConv(1024, 512)
+
+        self.up2 = Up(512, bilinear)
+        self.attention2 = Attention_block(256, 256, 128)
+        self.up_conv2 = DoubleConv(512, 256)
+
+        self.up3 = Up(256, bilinear)
+        self.attention3 = Attention_block(128, 128, 64)
+        self.up_conv3 = DoubleConv(256, 128)
+
+        self.up4 = Up(128, bilinear)
+        self.attention4 = Attention_block(64, 64, 32)
+        self.up_conv4 = DoubleConv(128, 64)
+
+        self.outc = OutConv(64, n_class)
+
+
+
+
+
 
 
     def forward(self, input, ids, mask, token_type_ids):
@@ -98,57 +127,53 @@ class ResAttNetUNet(nn.Module):
         print(f"layer3: {layer3.size()}")
         print(f"layer4: {layer4.size()}")
 
-        layer4 = self.layer4_1x1(layer4)
-        print(f"layer4 size after 1x1 {layer4.size()}")
+        #x5 = self.lang_attn(lang_rep=lang_rep, vision_rep=x5)
 
-        #print(f"after upsampling {layer4.size()}")
-        x = self.upsample(layer4)
-        print(f"x value after upsampling layer4 {x.size()}")
+        decode1 = self.up1(layer4)
 
-        test = self.up1(layer4)
-        print(f"test dimensions: {test.size()}")
+        #print("x5 shape")
+        #print(x5.size())
+        #x4 = self.attention1(decode1, x4)
+        #test = self.multiplicativeAttention(lang_output[1], decode1)
+        #x5 = torch.swapaxes(x5, 0, 1)
 
-        layer3 = self.layer3_1x1(layer3)
-        #print(f"layer3 after 1x1 {layer3.size()}")
-
-        #attention
-
-        #layer3 = self.attention1(layer3, x)
-        x = torch.cat([x, layer3], dim=1)
-        x = self.conv_up3(x)
-
-        x = self.upsample(x)
-        layer2 = self.layer2_1x1(layer2)
-        #attention goes here
-        #layer2 = self.attention2(layer2, x)
-
-        x = torch.cat([x, layer2], dim=1)
-        #print(f"conv_up2 shape: {x.size()}")
-        x = self.conv_up2(x)
-        x = self.upsample(x)
-        layer1 = self.layer1_1x1(layer1)
-
-        #layer1 = self.attention1(layer1, x)
-
-        x = torch.cat([x, layer1], dim=1)
-        x = self.conv_up1(x)
-
-        x = self.upsample(x)
-        layer0 = self.layer0_1x1(layer0)
-
-        #layer0 = self.attention3(layer0, x)
+        #x5 = torch.flatten(x5, start_dim=2)
+        #x5 = torch.swapaxes(x5, 2, 0)
 
 
-        x = torch.cat([x, layer0], dim=1)
-        x = self.conv_up0(x)
+        #lang_rep = torch.swapaxes(lang_rep, 0, 1)
+        #lang_rep = torch.swapaxes(lang_rep, 1, 2)
 
-        x = self.upsample(x)
-        x = torch.cat([x, x_original], dim=1)
-        x = self.conv_original_size2(x)
+        #print("lang_rep")
+        #print(lang_rep.size())
 
-        out = self.conv_last(x)
+        #test_att, test_other = self.multihead_attn(query = decode1, key = lang_rep, value = lang_rep)
 
-        return out
+        #test = self.multihead_attn(query=lang_rep, key=decode1, value=decode1)
+        #print("attention size")
+        #print(test_att.size())
+        #print(test_other.size())
+        x = concatenate_layers(decode1, layer3)
+        x = self.up_conv1(x)
+
+        decode2 = self.up2(x)
+        #x3 = self.attention2(decode2, x3)
+        x = concatenate_layers(decode2, layer2)
+        x = self.up_conv2(x)
+
+        decode3 = self.up3(x)
+        #x2 = self.attention3(decode3, x2)
+        x = concatenate_layers(decode3, layer1)
+        x = self.up_conv3(x)
+
+        decode4 = self.up4(x)
+        #x1 = self.attention4(decode4, x1)
+        x = concatenate_layers(decode4, layer0)
+        x = self.up_conv4(x)
+
+        logits = self.outc(x)
+
+        return logits
 
 
 class Attention_block(nn.Module):
@@ -200,3 +225,86 @@ class Up(nn.Module):
         x1 = self.up(x1)
 
         return x1
+
+
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+def concatenate_layers(x1, x2):
+
+    # input is CHW
+    diffY = x2.size()[2] - x1.size()[2]
+    diffX = x2.size()[3] - x1.size()[3]
+
+    x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                    diffY // 2, diffY - diffY // 2])
+
+    x = torch.cat([x2, x1], dim=1)
+    return x
+
+class LangCrossAtt(nn.Module):
+    "add documentaiton"
+
+
+    def __init__(self, emb_dim):
+        super(LangCrossAtt, self).__init__()
+
+        self.multihead_attn = nn.MultiheadAttention(embed_dim=emb_dim, num_heads=1)
+
+    def forward(self, lang_rep, vision_rep):
+
+        # gets all of the dimensions to be used in the attention
+        input_batch = vision_rep.size()[0]
+        input_channel =  vision_rep.size()[1]
+        input_width = vision_rep.size()[2]
+        input_height =  vision_rep.size()[3]
+
+        # puts the vision representation into the right shape for attention mechanism
+        vision_rep = torch.swapaxes(vision_rep, 0, 1)
+        vision_rep_flat = torch.flatten(vision_rep, start_dim=2)
+        vision_rep = torch.swapaxes(vision_rep_flat, 2, 0)
+
+        # puts the language rep into the right shape for attention
+        lang_rep = torch.swapaxes(lang_rep, 0, 1)
+        lang_rep = torch.swapaxes(lang_rep, 1, 2)
+
+        # does cross attention between vision and language
+        att_matrix, attn_output_weights = self.multihead_attn(query=vision_rep, key=lang_rep, value=lang_rep)
+
+        # gets the attention weights and repeats them to have the same size as the total channels
+        attn_output_weights = torch.swapaxes(attn_output_weights, 0, 1)
+        attn_output_weights = attn_output_weights.repeat(1, 1, input_channel)
+
+        # multiplies the attention to focus the vision rep based on the lang rep
+        vision_rep = vision_rep * attn_output_weights
+        vision_rep = vision_rep.contiguous()
+
+        # rearanges the output matrix to be the dimensions of the input
+        out = vision_rep.view(input_width, input_height, input_batch, input_channel)
+        out = torch.swapaxes(out, 0, 2)
+        out = torch.swapaxes(out, 1, 3)
+        return out
