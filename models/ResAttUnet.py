72ac68b5e75df481688eb48ff23b75d8842ab17d
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-06-23 16:03:26 -0500

    Testing resnet with spatial attention and lang attention encoder

--- models/ResAttUnet.py
+++ models/ResAttUnet.py
@@ -271,11 +271,12 @@ class LangCrossAtt(nn.Module):
         att_matrix, attn_output_weights = self.multihead_attn(query=lang_rep, key=vision_rep, value=vision_rep)
 
         print(f"attn output weights size {attn_output_weights.size()}")
-        print(f"attn matrix weights size {att_matrix.size()}")
+        #print(f"attn matrix weights size {att_matrix.size()}")
 
 
         # gets the attention weights and repeats them to have the same size as the total channels
-        # attn_output_weights = torch.swapaxes(attn_output_weights, 0, 1)
+        attn_output_weights = torch.swapaxes(attn_output_weights, 0, 1)
+        attn_output_weights = torch.swapaxes(attn_output_weights, 0, 2)
         attn_output_weights = attn_output_weights.repeat(1, 1, input_channel)
 
 
