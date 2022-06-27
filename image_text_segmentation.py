538ec30792798115f22f21aeafad8fbcaf58e4a8
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-06-27 12:57:03 -0500

    Language cross atttention in standard Unet evaluating freeze t5

--- image_text_segmentation.py
+++ image_text_segmentation.py
@@ -235,8 +235,6 @@ def train_image_text_segmentation(config, batch_size=8, epoch=1, dir_base = "/ho
     for param in language_model.parameters():
         param.requires_grad = False
 
-        print(param)
-
 
     test_obj = Attention_ConTEXTual_Seg_Model(lang_model=language_model, n_channels=3, n_classes=1, bilinear=False)
 
