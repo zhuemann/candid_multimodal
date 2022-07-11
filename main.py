ae990019444f925ac296b68069fe9b411bc5cdda
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-07-11 11:50:20 -0500

    Changed optimizer to AdamW, turned down augmentations, turned up learning rate, base Unet to be run on second gpu no augmentations with langauge

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ if __name__ == '__main__':
 
         df = pd.DataFrame(valid_log)
         df["test_accuracy"] = acc
-        folder_name = "Unet_baseline_no_augV5"
+        folder_name = "Unet_baseline_no_aug_with_lang_attentionV6"
         # save to xlsx file
         filepath = os.path.join(directory_base,
                                 '/UserData/Zach_Analysis/result_logs/candid_result/text_segmentation/stabalized_tests/' + str(
