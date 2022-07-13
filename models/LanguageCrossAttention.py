538dda8ce38d68b86d10e6ec57690ca861f33673
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-07-13 14:04:35 -0500

    Working on visualizing attention on dgx

--- models/LanguageCrossAttention.py
+++ models/LanguageCrossAttention.py
@@ -66,7 +66,8 @@ class LangCrossAtt(nn.Module):
 
         print(f"all the elements for one batch {np.shape(img)}")
 
-        img = np.reshape(img, (input_width, input_height))
+        #img = np.reshape(img, (input_width, input_height))
+        img = np.reshape(img, (input_batch, 1))
 
         max = np.amax(img)
         min = np.amin(img)
