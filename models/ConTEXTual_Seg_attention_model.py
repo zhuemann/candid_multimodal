5e2beb2ca9c146d727309f060910fa3637034e44
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-08-03 18:19:07 -0500

    Working on getting roberta to output word level embeddings

--- models/ConTEXTual_Seg_attention_model.py
+++ models/ConTEXTual_Seg_attention_model.py
@@ -62,10 +62,9 @@ class Attention_ConTEXTual_Seg_Model(torch.nn.Module):
     def forward(self, img, ids, mask, token_type_ids):
 
         # for roberta
-        lang_output= self.lang_encoder(ids, mask, token_type_ids)
-        for i in range(0,len(lang_output)):
-            print(lang_output[i].size())
-        print(lang_output.size())
+        lang_output = self.lang_encoder(ids, mask, token_type_ids)
+
+
         lang_rep = lang_output[1]
         print(lang_output.size())
 
