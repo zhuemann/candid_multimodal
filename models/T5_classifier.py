a9fc6a1e15b3b9d3e0f0ea18a25f725124bc2688
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-08-03 18:02:57 -0500

    Working on getting roberta to output word level embeddings

--- models/T5_classifier.py
+++ models/T5_classifier.py
@@ -9,10 +9,16 @@ class T5Classifier(torch.nn.Module):
 
     def forward(self, ids, mask, token_type_ids):
 
+        lang_output = self.lang_encoder(ids, mask, token_type_ids)
+        print(lang_output.size())
+        lang_rep = lang_output[1]
+        print(lang_rep.size())
+        output = lang_rep
+
         # feed text through t5 then average across encoding dimension and then do two class classification
-        encoder_output = self.lang_encoder.encoder(input_ids=ids, attention_mask=mask, return_dict=True)
-        pooled_sentence = encoder_output.last_hidden_state
-        lang_rep_avg = torch.mean(pooled_sentence, 1)
-        output = self.classifier(lang_rep_avg)
+        #encoder_output = self.lang_encoder.encoder(input_ids=ids, attention_mask=mask, return_dict=True)
+        #pooled_sentence = encoder_output.last_hidden_state
+        #lang_rep_avg = torch.mean(pooled_sentence, 1)
+        #output = self.classifier(lang_rep_avg)
 
         return output
