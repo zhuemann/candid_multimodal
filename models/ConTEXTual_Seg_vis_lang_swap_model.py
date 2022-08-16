1247980c5307b9fac533cef5529b112f890f88fd
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-08-15 21:57:54 -0500

    Adding second langauge attention

--- models/ConTEXTual_Seg_vis_lang_swap_model.py
+++ models/ConTEXTual_Seg_vis_lang_swap_model.py
@@ -89,10 +89,12 @@ class Attention_ConTEXTual_Seg_Model_swap(torch.nn.Module):
         print(f"decode1 size: {decode1.size()}")
         print(f"lang_rep1 size: {lang_rep1.size()}")
         decode1 = self.lang_attn1d(lang_rep=lang_rep1, vision_rep=decode1)
+        print(f"decode after: {decode1.size()}")
         print(f"x4 size: {x4.size()}")
         lang_rep1x = self.lang_proj1x(lang_rep)
         print(f"lang_rep1x: {lang_rep1x.size()}")
         x4 = self.lang_attn1x(lang_rep=lang_rep1x, vision_rep=x4)
+        print(f"x4 size after {x4.size()}")
         # How is used to be done, swapping for testing
         x4 = self.attention1(decode1, x4)
 
