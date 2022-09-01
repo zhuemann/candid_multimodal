f29f60288deccb768675e782083b92dd26e19b5d
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-09-01 18:38:14 -0500

    Creating the attention visualizations

--- visualization_attention.py
+++ visualization_attention.py
@@ -134,9 +134,9 @@ def colorize_img(img):
     colorized_img_neg = np.expand_dims(colorized_img_neg, axis=2)
     third_dimension = np.expand_dims(third_dimension, axis=2)
 
-    colorized_img = np.concatenate((colorized_img_pos, third_dimension), axis=2)
-    colorized_img = np.concatenate((colorized_img,  colorized_img_neg), axis = 2)
-    #colorized_img = np.concatenate((colorized_img, third_dimension), axis=2)
+    colorized_img = np.concatenate((colorized_img_pos, colorized_img_neg), axis=2)
+    #colorized_img = np.concatenate((colorized_img,  colorized_img_neg), axis = 2)
+    colorized_img = np.concatenate((colorized_img, third_dimension), axis=2)
     print(f"coloriaed_img size: {colorized_img.shape}")
 
     return colorized_img
