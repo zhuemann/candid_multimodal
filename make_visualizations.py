79966d16c78a2857d11dea24df89ac8df81063be
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-09-02 12:34:38 -0500

    Make bilinear interp visualizations

--- make_visualizations.py
+++ make_visualizations.py
@@ -138,9 +138,12 @@ def make_images_on_dgx(config, batch_size=8, epoch=1, dir_base = "/home/zmh001/r
     #test_dataframe_location = os.path.join(dir_base, 'Zach_Analysis/candid_data/pneumothorax_df_testset.xlsx')
     #test_df.to_excel(test_dataframe_location, index=True)
 
+    #test_frame_locaction = os.path.join(dir_base,
+    #"Zach_Analysis/result_logs/candid_result/image_text_segmentation_for_paper/with_augmentation/" +
+    #"t5_vis_attention_positive_cases_with_bilinear_interp_v11/seed98/pneumothorax_testset_df_seed98.xlsx")
     test_frame_locaction = os.path.join(dir_base,
     "Zach_Analysis/result_logs/candid_result/image_text_segmentation_for_paper/with_augmentation/" +
-    "t5_vis_attention_positive_cases_with_bilinear_interp_v11/seed98/pneumothorax_testset_df_seed98.xlsx")
+    "t5_vis_attention_positive_cases_with_bilinear_interp_v11/seed98/singular_case_for_vis.xlsx")
     test_df = pd.read_excel(test_frame_locaction, engine='openpyxl')
     test_df.set_index("image_id", inplace=True)
 
