10659af3b702c7eb77e471dcc7c58cb19e26c57f
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-09-02 13:58:09 -0500

    Make bilinear interp visualizations

--- dataloader_image_text.py
+++ dataloader_image_text.py
@@ -44,7 +44,8 @@ class TextImageDataset(Dataset):
         text = str(self.text[index])
 
         #text = " Clinical data: [PERSONALNAME] pain, reduced air entry left side, no trauma.; Medical question: ? spontaneous pneumothorax.;  [PERSONALNAME] There is a large left-sided pneumothorax.The mediastinum is shifted mildly to the right.The right lung appears normal.The heart size is within normal limits. No other abnormality is seen. Reported in consultation with Dr [PERSONALNAME]. Transcribed by:  wk Dr [PERSONALNAME]  Dr. [PERSONALNAME] [PERSONALNAME] "
-        text = "There is a large left-sided pneumothorax.The mediastinum is shifted mildly to the right.The right lung appears normal.The heart size is within normal limits. No other abnormality is seen."
+        #text = "There is a large left-sided pneumothorax. The mediastinum is shifted mildly to the right.The right lung appears normal.The heart size is within normal limits. No other abnormality is seen."
+        text = "There is a large left-sided pneumothorax."
         text = " ".join(text.split())
         # print(text)
 
