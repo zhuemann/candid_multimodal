98655debb4f1d302c41e3d83d8530dd6b97c1c4f
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-09-08 15:46:59 -0500

    vis test double pnuemo

--- make_visualizations.py
+++ make_visualizations.py
@@ -61,7 +61,7 @@ def make_images_on_dgx(config, batch_size=8, epoch=1, dir_base = "/home/zmh001/r
     BATCH_SIZE = config["batch_size"]
     BATCH_SIZE = 1
     N_EPOCHS = config["epochs"]
-    seed = 456
+    seed = 915
     dataframe_location = os.path.join(dir_base, 'Zach_Analysis/candid_data/pneumothorax_with_multisegmentation_positive_text_df.xlsx')
     #dataframe_location = os.path.join(dir_base, 'Zach_Analysis/candid_data/pneumothorax_with_text_df.xlsx') #pneumothorax_df chest_tube_df rib_fracture
     #dataframe_location = os.path.join(dir_base, 'Zach_Analysis/candid_data/pneumothorax_large_df.xlsx')
