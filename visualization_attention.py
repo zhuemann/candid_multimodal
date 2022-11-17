67cbc8b5b30b4c5def84d3a50acdd16843267c67
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-11-17 15:34:05 -0600

    visualization run

--- visualization_attention.py
+++ visualization_attention.py
@@ -91,11 +91,11 @@ def visualization_attention(img, vision_rep_before, vision_rep, lang_rep, att_ma
 
     #print(f"img test shape before: {img_test.shape}")
     print(np.sum(model_output)/255)
-    img_test[:,:,0] += (target_batch_unnorm[0,0,:,:]*(255/3)/np.amax(target_batch_unnorm[0,0,:,:]))
+    #img_test[:,:,0] += (target_batch_unnorm[0,0,:,:]*(255/3)/np.amax(target_batch_unnorm[0,0,:,:]))
     img_test[:,:,2] += model_output[0, 0, :, :]/3
     #print(f"img test shape after: {img_test.shape}")
     fullpath = os.path.join(dir_base,
-                            'Zach_Analysis/dgx_images/' + save_folder + '/test_input_overlay' + '.png')
+                            'Zach_Analysis/dgx_images/' + save_folder + '/output_overlay' + '.png')
     cv2.imwrite(fullpath, img_test)
 
     # visualizes the attention matrices
