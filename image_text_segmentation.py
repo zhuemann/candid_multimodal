2e79569935ef87fd11b7cf3beaf4c45a68b9e63d
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-12-01 13:48:47 -0600

    language attention no text aug 1024x1024

--- image_text_segmentation.py
+++ image_text_segmentation.py
@@ -253,7 +253,7 @@ def train_image_text_segmentation(config, batch_size=8, epoch=1, dir_base = "/ho
 
     train_params = {'batch_size': BATCH_SIZE,
                 'shuffle': True,
-                'num_workers': 4
+                'num_workers': 8
                 }
 
     test_params = {'batch_size': BATCH_SIZE,
