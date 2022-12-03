f99884a0196d1665e9e74f96a5f395305a49b2c2
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-12-03 14:52:01 -0600

    T5 with setence shuffle

--- image_text_segmentation.py
+++ image_text_segmentation.py
@@ -253,7 +253,7 @@ def train_image_text_segmentation(config, batch_size=8, epoch=1, dir_base = "/ho
 
     train_params = {'batch_size': BATCH_SIZE,
                 'shuffle': True,
-                'num_workers': 6
+                'num_workers': 4
                 }
 
     test_params = {'batch_size': BATCH_SIZE,
