4248090ec52ee5e4ea82642d09ec6326e09eb5fa
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-12-04 00:19:51 -0600

    baseline unet no augmentation higher res

--- image_text_segmentation.py
+++ image_text_segmentation.py
@@ -308,8 +308,9 @@ def train_image_text_segmentation(config, batch_size=8, epoch=1, dir_base = "/ho
     #test_obj = Unet_Baseline(n_channels=3, n_classes=1, bilinear=True)
     #test_obj = ResAttNetUNet(lang_model=language_model, n_class=1, dir_base=dir_base)
 
-    for param in language_model.parameters():
-        param.requires_grad = False
+    print("need to unfreeze lang params")
+    #for param in language_model.parameters():
+    #    param.requires_grad = False
 
 
     #test_obj = Attention_ConTEXTual_Seg_Model(lang_model=language_model, n_channels=3, n_classes=1, bilinear=False)
