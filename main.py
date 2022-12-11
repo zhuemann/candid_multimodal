6d7b6848f94e6aa75397b9c5273095d49170aefa
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-12-10 18:16:37 -0600

    Baseline Unet no aug

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     config = {"seed": 1, "batch_size": 8, "dir_base": directory_base, "epochs": 150, "n_classes": 2, "LR": 1e-5,
               "IMG_SIZE": 256, "train_samples": .8, "test_samples": .5, "data_path": "D:/candid_ptx/", "report_gen":False, "mlm_pretraining":False, "contrastive_training": True}
 
-    config = {"seed": 1, "batch_size": 1, "dir_base": directory_base, "epochs": 100, "n_classes": 2, "LR": 1e-3,
+    config = {"seed": 1, "batch_size": 2, "dir_base": directory_base, "epochs": 100, "n_classes": 2, "LR": 1e-3,
               "IMG_SIZE": 1024, "train_samples": .8, "test_samples": .5, "data_path": "D:/candid_ptx/",
               "report_gen": False, "mlm_pretraining": False, "contrastive_training": True, "save_location": ""} #batch size was 8 with image size 256 .8 can use .004
 
@@ -108,7 +108,7 @@ if __name__ == '__main__':
 
         #folder_name = "with_augmentation/attention_unet_frozen_positive_cases_all_aug_100flip/seed" + str(seed) + "/"
         #folder_name = "higher_res_for_paper/t5_language_att_with_setence_shuffle_larger_img_v27/seed" + str(seed) + "/"
-        folder_name = "higher_res_for_paper/roberta_language_att_with_text_aug_larger_img_v25/seed" + str(seed) + "/"
+        folder_name = "higher_res_for_paper/baseline_unet_no_aug_larger_img_v28/seed" + str(seed) + "/"
 
         #folder_name = "higher_res_for_paper/baseline_unet_no_aug_larger_img_v28/seed" + str(seed) + "/"
 
