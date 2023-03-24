3c109efb8665c2d8447072b4d4e7919c050086e4
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-03-24 12:40:44 -0500

    working on getting LAVT running

--- image_text_segmentation.py
+++ image_text_segmentation.py
@@ -370,9 +370,9 @@ def train_image_text_segmentation(config, args , batch_size=8, epoch=1, dir_base
             images = data['images'].to(device, dtype=torch.float)
 
             #print(images.size())
-            #outputs = test_obj(images, ids, mask)  # for lavt
+            outputs = test_obj(images, ids, mask)  # for lavt
             #outputs = test_obj(images, ids, mask, token_type_ids)
-            outputs = test_obj(images)
+            #outputs = test_obj(images)
             #outputs = model_obj(images)
             #print(outputs.size())
             outputs = output_resize(torch.squeeze(outputs, dim=1))
@@ -424,9 +424,9 @@ def train_image_text_segmentation(config, args , batch_size=8, epoch=1, dir_base
                 images = data['images'].to(device, dtype=torch.float)
 
                 #outputs = model_obj(images)
-                #outputs = test_obj(images, ids, mask)  # for lavt
+                outputs = test_obj(images, ids, mask)  # for lavt
                 #outputs = test_obj(images, ids, mask, token_type_ids)
-                outputs = test_obj(images)
+                #outputs = test_obj(images)
 
                 outputs = output_resize(torch.squeeze(outputs, dim=1))
                 #outputs = torch.squeeze(outputs)
@@ -492,9 +492,9 @@ def train_image_text_segmentation(config, args , batch_size=8, epoch=1, dir_base
             images = data['images'].to(device, dtype=torch.float)
 
             #outputs = model_obj(images)
-            #outputs = test_obj(images, ids, mask) #for lavt
+            outputs = test_obj(images, ids, mask) #for lavt
             #outputs = test_obj(images, ids, mask, token_type_ids) #for contextual net
-            outputs = test_obj(images)
+            #outputs = test_obj(images)
 
             outputs = output_resize(torch.squeeze(outputs, dim=1))
             #outputs = outputs.squeeze(outputs)
