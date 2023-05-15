1869a79d9e32f9c14e16e751f9feae53da1121d4
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-05-15 16:14:35 -0500

    Rerunning with Tanh v40

--- image_text_segmentation.py
+++ image_text_segmentation.py
@@ -526,6 +526,8 @@ def train_image_text_segmentation(config, args , batch_size=8, epoch=1, dir_base
             row_ids.extend(data['row_ids'])
 
             for i in range(0, outputs.shape[0]):
+                print(outputs[i].item())
+                print(type(outputs[i].item()))
                 pred_rle = mask2rle(outputs[i].item())
                 target_rle= mask2rle(targets[i].item())
                 ids_example = ids[i].item()
