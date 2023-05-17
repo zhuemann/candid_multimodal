160da15e42dbeba8849b03b20270cff79cbcfe8d
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-05-17 09:43:40 -0500

    Rerunning with Tanh v40 checking for nan values

--- models/ConTextual_seg_lang_model.py
+++ models/ConTextual_seg_lang_model.py
@@ -68,6 +68,8 @@ class Attention_ConTEXTual_Lang_Seg_Model(torch.nn.Module):
         pooled_sentence = encoder_output.last_hidden_state
         lang_rep = pooled_sentence
 
+        print(torch.isnan(lang_rep).any().item())
+
         # print(f"size img: {img.size()}")
         x1 = self.inc(img)
         x2 = self.down1(x1)
