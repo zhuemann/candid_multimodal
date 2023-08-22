2531177cc7ffdd88ab49bad0979c2d84e9227368
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-08-22 16:22:17 -0500

    Try making images, of smp u-net outputs

--- make_visualizations.py
+++ make_visualizations.py
@@ -55,7 +55,7 @@ def make_images_on_dgx(config, batch_size=8, epoch=1, dir_base = "/home/zmh001/r
     nltk.download('punkt')
     # model specific global variables
     IMG_SIZE = config["IMG_SIZE"] #256 #1024 #512 #384
-    IMG_SIZE = 256
+    IMG_SIZE = 1024
     #BATCH_SIZE = batch_size
     LR = 5e-5 #8e-5  # 1e-4 was for efficient #1e-06 #2e-6 1e-6 for transformer 1e-4 for efficientnet
     N_EPOCHS = epoch
