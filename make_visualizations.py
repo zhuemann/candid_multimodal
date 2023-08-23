5e2263cbadfb197f8dc4e84cf9a754ac46e1447e
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-08-23 17:30:09 -0500

    Try making images, of smp u-net outputs

--- make_visualizations.py
+++ make_visualizations.py
@@ -308,9 +308,9 @@ def make_images_on_dgx(config, batch_size=8, epoch=1, dir_base = "/home/zmh001/r
                 model_output = outputs.cpu().detach().numpy()
                 output_overlay = (model_output * 255) / np.amax(model_output)
 
-                print(output_overlay.shape())
+                print(output_overlay.shape)
                 np.squeeze(output_overlay)
-                print(output_overlay.shape())
+                print(output_overlay.shape)
                 img_overlay[:, :, 1] += output_overlay
                 #img_overlay[:, :, 1] += output_overlay[j, 0, :, :]
 
