e2cf7e75dedac90d0b74871f09613b2165acde96
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-10-31 21:14:01 -0500

    First try at having encoder attention v61

--- models/ConTextual_seg_lang_model_encoder_attention.py
+++ models/ConTextual_seg_lang_model_encoder_attention.py
@@ -57,8 +57,8 @@ class Attention_ConTEXTual_Lang_Seg_Model(torch.nn.Module):
         self.lang_proj4 = nn.Linear(lang_dimension, 64)
         self.lang_attn4 = LangCrossAtt(emb_dim=64)
 
-        self.lang_proj5_encoder = nn.Linear(lang_dimension, 1024)
-        self.lang_attn5_encoder = LangCrossAtt(emb_dim=1024)
+        #self.lang_proj5_encoder = nn.Linear(lang_dimension, 1024)
+        #self.lang_attn5_encoder = LangCrossAtt(emb_dim=1024)
         self.lang_proj4_encoder = nn.Linear(lang_dimension, 512)
         self.lang_attn4_encoder = LangCrossAtt(emb_dim=512)
         self.lang_proj3_encoder = nn.Linear(lang_dimension, 256)
@@ -105,17 +105,17 @@ class Attention_ConTEXTual_Lang_Seg_Model(torch.nn.Module):
 
         x5 = self.down4(x4)
 
-        print(f"x5 size: {x5.size()}")
+        #print(f"x5 size: {x5.size()}")
 
-        lang_rep5_encoder = self.lang_proj5_encoder(lang_rep)
-        x5 = self.lang_attn5_encoder(lang_rep=lang_rep5_encoder, vision_rep=x5)
+        #lang_rep5_encoder = self.lang_proj5_encoder(lang_rep)
+        #x5 = self.lang_attn5_encoder(lang_rep=lang_rep5_encoder, vision_rep=x5)
 
         decode1 = self.up1(x5)
 
         lang_rep1 = self.lang_proj1(lang_rep)
         decode1 = self.lang_attn1(lang_rep=lang_rep1, vision_rep=decode1)
 
-        print(f"decode1 size: {decode1.size()}")
+        #print(f"decode1 size: {decode1.size()}")
 
         # How is used to be done, swapping for testing
         #x4 = self.attention1(decode1, x4)
