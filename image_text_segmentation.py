621d90b6dd6a17aa859237f79d0413d893bd303c
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-11-09 21:19:28 -0600

    swin baseline work in progress v69

--- image_text_segmentation.py
+++ image_text_segmentation.py
@@ -68,7 +68,7 @@ def train_image_text_segmentation(config, args , batch_size=8, epoch=1, dir_base
     # model specific global variables
     IMG_SIZE = config["IMG_SIZE"] #256 #1024 #512 #384
     #BATCH_SIZE = batch_size
-    LR = 8e-4 #5e-5 #5e-5 was lr for contextualnet runs #8e-5  # 1e-4 was for efficient #1e-06 #2e-6 1e-6 for transformer 1e-4 for efficientnet
+    LR = 1e-4 #5e-5 #5e-5 was lr for contextualnet runs #8e-5  # 1e-4 was for efficient #1e-06 #2e-6 1e-6 for transformer 1e-4 for efficientnet
     #LR = 5e-4
     N_EPOCHS = epoch
     N_CLASS = n_classes
@@ -390,7 +390,7 @@ def train_image_text_segmentation(config, args , batch_size=8, epoch=1, dir_base
 
     # defines which optimizer is being used
     optimizer = torch.optim.AdamW(params=test_obj.parameters(), lr=LR)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=127800, eta_min=8e-6, last_epoch=-1, verbose=False)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=127800, eta_min=20e-6, last_epoch=-1, verbose=False)
 
     #optimizer = torch.optim.Adam(params=test_obj.parameters(), lr=LR) # was used for all the baselines
     #optimizer_vis = torch.optim.Adam(params = vision_model.parameters(), lr=LR, weight_decay=1e-6)
